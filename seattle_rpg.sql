19a3598ac09a2d0acbc80bbe4c76ff82f7134b0e
Alex Noble <AWGR-Alex@comcast.net>
2018-05-14 14:12:41 -0700

    add updated database

--- seattle_rpg.sql
+++ seattle_rpg.sql
@@ -3,11 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
-<<<<<<< HEAD
--- Generation Time: May 14, 2018 at 08:24 PM
-=======
--- Generation Time: May 14, 2018 at 10:11 PM
->>>>>>> 46aebe5ce187c8be94f7cbcd109c257e1e745c80
+-- Generation Time: May 14, 2018 at 11:12 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -38,36 +34,27 @@ CREATE TABLE `scenarios` (
   `prompt_text` varchar(255) NOT NULL,
   `opt_1_choice_text` varchar(255) NOT NULL,
   `opt_1_result_text` varchar(255) NOT NULL,
-<<<<<<< HEAD
-  `option_1_health_effect` varchar(255) NOT NULL,
-  `opt_1_money_effect` varchar(255) NOT NULL,
-  `opt_1_mood_effect` varchar(255) NOT NULL,
-  `opt_2_choice_text` varchar(255) NOT NULL,
-  `opt_2_result_text` varchar(255) NOT NULL,
-  `opt_2_health` varchar(255) NOT NULL,
-  `opt_2_money` varchar(255) NOT NULL,
-  `opt_2_mood` varchar(255) NOT NULL,
-  `opt_3_choice_text` varchar(255) NOT NULL,
-  `opt_3_result_text` varchar(255) NOT NULL,
-  `opt_3_money` varchar(255) NOT NULL,
-  `opt_3_mood` varchar(255) NOT NULL,
-  `opt_3_health` varchar(255) NOT NULL
-=======
   `opt_1_health` int(11) NOT NULL,
-  `opt_1_money` float NOT NULL,
+  `opt_1_money` int(11) NOT NULL,
   `opt_1_mood` int(11) NOT NULL,
   `opt_2_choice_text` varchar(255) NOT NULL,
   `opt_2_result_text` varchar(255) NOT NULL,
   `opt_2_health` int(11) NOT NULL,
-  `opt_2_money` float NOT NULL,
+  `opt_2_money` int(11) NOT NULL,
   `opt_2_mood` int(11) NOT NULL,
   `opt_3_choice_text` varchar(255) NOT NULL,
   `opt_3_result_text` varchar(255) NOT NULL,
   `opt_3_health` int(11) NOT NULL,
-  `opt_3_money` float NOT NULL,
+  `opt_3_money` int(11) NOT NULL,
   `opt_3_mood` int(11) NOT NULL
->>>>>>> 46aebe5ce187c8be94f7cbcd109c257e1e745c80
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `scenarios`
+--
+
+INSERT INTO `scenarios` (`id`, `scenario_name`, `prompt_text`, `opt_1_choice_text`, `opt_1_result_text`, `opt_1_health`, `opt_1_money`, `opt_1_mood`, `opt_2_choice_text`, `opt_2_result_text`, `opt_2_health`, `opt_2_money`, `opt_2_mood`, `opt_3_choice_text`, `opt_3_result_text`, `opt_3_health`, `opt_3_money`, `opt_3_mood`) VALUES
+(1, 'Dream Job', 'You got your dream job offer!', 'Decline', 'Keep on dreaming! \r\n-1 to mood.', 0, 0, -1, 'Random', 'You got a your dream job with a relocation bonus! +2 to health. +500 to money. +10 to mood.', 2, 500, 10, 'Accept', '\"Yay!\"', 0, 1000, 8);
 
 --
 -- Indexes for dumped tables
@@ -87,7 +74,7 @@ ALTER TABLE `scenarios`
 -- AUTO_INCREMENT for table `scenarios`
 --
 ALTER TABLE `scenarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
