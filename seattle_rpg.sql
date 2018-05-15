70fd0aedd3250bc9babe37ee414fbbd1e97114e7
ab <azamat.bekmuratov24@gmail.com>
2018-05-15 11:44:51 -0700

    add new player table to db

--- seattle_rpg.sql
+++ seattle_rpg.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 14, 2018 at 11:12 PM
+-- Generation Time: May 15, 2018 at 08:43 PM
 -- Server version: 5.6.35
 -- PHP Version: 7.0.15
 
@@ -25,6 +25,20 @@ USE `seattle_rpg`;
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `current_player`
+--
+
+CREATE TABLE `current_player` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `health` int(11) NOT NULL,
+  `mood` int(11) NOT NULL,
+  `money` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `scenarios`
 --
 
@@ -34,9 +48,9 @@ CREATE TABLE `scenarios` (
   `prompt_text` varchar(255) NOT NULL,
   `opt_1_choice_text` varchar(255) NOT NULL,
   `opt_1_result_text` varchar(255) NOT NULL,
-  `opt_1_health` int(11) NOT NULL,
-  `opt_1_money` int(11) NOT NULL,
-  `opt_1_mood` int(11) NOT NULL,
+  `option_1_health_effect` int(11) NOT NULL,
+  `opt_1_money_effect` int(11) NOT NULL,
+  `opt_1_mood_effect` int(11) NOT NULL,
   `opt_2_choice_text` varchar(255) NOT NULL,
   `opt_2_result_text` varchar(255) NOT NULL,
   `opt_2_health` int(11) NOT NULL,
@@ -44,21 +58,20 @@ CREATE TABLE `scenarios` (
   `opt_2_mood` int(11) NOT NULL,
   `opt_3_choice_text` varchar(255) NOT NULL,
   `opt_3_result_text` varchar(255) NOT NULL,
-  `opt_3_health` int(11) NOT NULL,
   `opt_3_money` int(11) NOT NULL,
-  `opt_3_mood` int(11) NOT NULL
+  `opt_3_mood` int(11) NOT NULL,
+  `opt_3_health` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Dumping data for table `scenarios`
---
-
-INSERT INTO `scenarios` (`id`, `scenario_name`, `prompt_text`, `opt_1_choice_text`, `opt_1_result_text`, `opt_1_health`, `opt_1_money`, `opt_1_mood`, `opt_2_choice_text`, `opt_2_result_text`, `opt_2_health`, `opt_2_money`, `opt_2_mood`, `opt_3_choice_text`, `opt_3_result_text`, `opt_3_health`, `opt_3_money`, `opt_3_mood`) VALUES
-(1, 'Dream Job', 'You got your dream job offer!', 'Decline', 'Keep on dreaming! \r\n-1 to mood.', 0, 0, -1, 'Random', 'You got a your dream job with a relocation bonus! +2 to health. +500 to money. +10 to mood.', 2, 500, 10, 'Accept', '\"Yay!\"', 0, 1000, 8);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `current_player`
+--
+ALTER TABLE `current_player`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `scenarios`
@@ -70,6 +83,11 @@ ALTER TABLE `scenarios`
 -- AUTO_INCREMENT for dumped tables
 --
 
+--
+-- AUTO_INCREMENT for table `current_player`
+--
+ALTER TABLE `current_player`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `scenarios`
 --
